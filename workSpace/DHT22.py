d1080fbc414fb813d0bc2cdcaa735e2224c53bd9
DESKTOP-IG5BHNH\USER <ewen06263@gmail.com>
2019-04-17 22:01:42 +0800

    完成DHT22程式(深度睡眠)

---
+++ workSpace/DHT22.py
@@ -0,0 +1,66 @@
+"""
+{"name":"測試溫度","characteristic":"CurrentTemperature","value":18}
+
+"""
+from machine import Pin
+from umqtt.simple import MQTTClient
+import dht
+import json
+import time
+import machine
+import network
+import ubinascii
+
+WiFi_SSID = "dlink-731"
+WiFi_PASS = "1223334444"
+MQTT_Server = "192.168.0.112"
+DeviceName = "Temp"
+data = {}
+
+sta = network.WLAN(network.STA_IF) #設定WiFi連線
+sta.active(True)
+sta.connect(WiFi_SSID,WiFi_PASS)
+mac = ubinascii.hexlify(sta.config('mac'),':').decode()#取得ADDRESS
+print(mac)
+while not sta.isconnected():
+  pass
+print(sta.ifconfig())
+
+
+d = dht.DHT22(Pin(2))
+
+rtc = machine.RTC()
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+rtc.alarm(rtc.ALARM0, 20000)
+
+time.sleep(2)
+
+
+
+
+d.measure()
+temp = d.temperature()
+hum = d.humidity()
+data['name'] = DeviceName
+data['characteristic'] = 'CurrentTemperature'
+data['value'] = temp
+data = json.dumps(data)
+print(data)
+print("----------------------------")
+print('Humidity: {}%'.format(hum))
+print('Temperature: {}{}C'.format(temp, '\u00b0'))
+print("GO TO SLEEP")
+print("----------------------------")
+
+
+c = MQTTClient("SuperTang" + mac, MQTT_Server)
+
+c.connect()
+
+c.publish(b"homebridge/to/set", data)
+
+
+c.disconnect()
+print("published!")
+time.sleep(1)
+machine.deepsleep()
--- workSpace/MQTT_Sample_PUB.py
+++ workSpace/MQTT_Sample_PUB.py
@@ -1,17 +1,19 @@
 
-    
+
 from umqtt.simple import MQTTClient
 
 # Test reception e.g. with:
 # mosquitto_sub -t foo_topic
 
-def main(server="test.mosquitto.org"):
+def main(server="192.168.0.112"):
     c = MQTTClient("umqtt_client", server)
     c.connect()
-    c.publish(b"homebridge/from/set", b"hello")
+    c.publish(b"homebridge/to/set", b"中文")
     c.publish(b"homebridge/from/set", b"hello1")
     c.disconnect()
 
 if __name__ == "__main__":
     main()
+
+
 
