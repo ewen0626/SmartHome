9dc11c8fb913b89722af986f9ee978b0d5cd93c0
DESKTOP-IG5BHNH\USER <ewen06263@gmail.com>
2019-06-13 23:57:38 +0800

    增加溫溼度同時上傳(WeatherStation)

--- workSpace/WeatherStation/DHT22.py
+++ workSpace/WeatherStation/DHT22.py
@@ -1,6 +1,6 @@
 """
 {"name":"測試溫度","characteristic":"CurrentTemperature","value":18}
-
+{"name": "客廳濕度", "service_name": "客廳濕度", "characteristic": "CurrentRelativeHumidity", "value": 85}
 """
 from machine import Pin
 from umqtt.simple import MQTTClient
@@ -11,10 +11,11 @@ import machine
 import network
 import ubinascii
 
-WiFi_SSID = "5678"
+WiFi_SSID = "SuperTang"
 WiFi_PASS = ""
-MQTT_Server = "192.168.0.199"
-DeviceName = "Temp"
+MQTT_Server = "192.168.1.112"
+DeviceName_temp = "客廳溫度"
+DeviceName_hum = "客廳濕度"
 data = {}
 ap = network.WLAN(network.AP_IF)
 ap.active(False)
@@ -34,7 +35,7 @@ d = dht.DHT22(Pin(5))
 
 rtc = machine.RTC()
 rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
-rtc.alarm(rtc.ALARM0, 30000)
+rtc.alarm(rtc.ALARM0, 20000)
 
 time.sleep(2)
 
@@ -44,24 +45,27 @@ try:
   time.sleep_ms(50)
   temp = d.temperature()
   hum = d.humidity()
-  data['name'] = DeviceName
-  data['characteristic'] = 'CurrentTemperature'
-  data['value'] = temp
-  data = json.dumps(data)
+  data_temp['name'] = DeviceName_temp
+  data_temp['characteristic'] = 'CurrentTemperature'
+  data_temp['value'] = temp
+  data_temp = json.dumps(data_temp)
+  
+  data_hum['name'] = DeviceName_hum
+  data_hum['characteristic'] = 'CurrentRelativeHumidity'
+  data_hum['value'] = hum
+  data_hum = json.dumps(data_hum) 
+  
   print(data)
   print("----------------------------")
   print('Humidity: {}%'.format(hum))
   print('Temperature: {}{}C'.format(temp, '\u00b0'))
-  
   print("----------------------------")
 
 
   c = MQTTClient("SuperTang" + mac, MQTT_Server)
-
   c.connect()
-
-  c.publish(b"homebridge/to/set", data)
-
+  c.publish(b"homebridge/to/set", data_hum)
+  c.publish(b"homebridge/to/set", data_temp)
 
   c.disconnect()
   print("published!")
@@ -72,6 +76,8 @@ except:
 time.sleep(1)
 print("GO TO SLEEP")
 machine.deepsleep()
+
+
 
 
 
